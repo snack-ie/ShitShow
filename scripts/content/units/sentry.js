8bed6f1d89982ae856abd609a1fbd386dcc74450
Pietro303HD <pietro303hd@gmail.com>
2021-08-10 23:23:19 -0300

    oopsie

--- scripts/content/units/sentry.js
+++ scripts/content/units/sentry.js
@@ -2,8 +2,7 @@ const sentry = extend(UnitType, "sentry", {
     type: "Ground",
     speed: 0,
     rotateSpeed: 1,
-    rotateShooting: false,
-    weapons: []
+    rotateShooting: false
 });
 
 sentry.constructor = () => extend(UnitEntity, {
